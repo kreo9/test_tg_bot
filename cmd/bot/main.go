5a7b012454af8305ef20b4823283de487762c90b
Oleg Iurov <olegurov485@gmail.com>
2023-01-23 15:24:16 +0300

    Добавлена логика работы бота

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Bot started")
+
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	//bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			//msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Я тупой бот, который только и умеет здороваться. Привет "+update.Message.From.UserName+".")
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
